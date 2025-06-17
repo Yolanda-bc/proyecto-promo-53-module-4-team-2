4542e63ac2c1f0dd929fae5f87c055d9913ed361
Yolanda-bc <jolandabc89@gmail.com>
2025-06-17 13:24:09 +0200

    add previewCard

--- FRONTEND-REACT/src/components/Pages/Landing.jsx
+++ FRONTEND-REACT/src/components/Pages/Landing.jsx
@@ -1,9 +1,40 @@
 import Hero from "../Layout/Hero";
+import PreviewCard from "../Projects/PreviewCard";
 function Landing() {
   return (
     <>
       {" "}
       <Hero />
+      <ul>
+        <li>
+          <PreviewCard
+            name="proyecto 1"
+            demo="htpps://google.es/"
+            repo="htpps://github.com/"
+          />
+        </li>
+        <li>
+          <PreviewCard
+            name="proyecto 2"
+            demo="htpps://google.es/"
+            repo="htpps://github.com/"
+          />
+        </li>
+        <li>
+          <PreviewCard
+            name="proyecto 3"
+            demo="htpps://google.es/"
+            repo="htpps://github.com/"
+          />
+        </li>
+        <li>
+          <PreviewCard
+            name="proyecto 4"
+            demo="htpps://google.es/"
+            repo="htpps://github.com/"
+          />
+        </li>
+      </ul>
     </>
   );
 }
